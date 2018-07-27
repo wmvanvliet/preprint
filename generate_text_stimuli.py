112ecb9189146aa190fe75427d2af3db57585e65
Marijn van Vliet <w.m.vanvliet@gmail.com>
2018-07-27 17:27:35 +0300

    Initial model: vocab of 1000 words

---
+++ generate_text_stimuli.py
@@ -0,0 +1,72 @@
+# encoding: utf-8
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+from os import makedirs
+import os.path as op
+from tqdm import tqdm
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Generate text stimuli')
+parser.add_argument('path', type=str, help='The path to write the dataset to.')
+parser.add_argument('set', type=str, help='Specify either "train" to generate the training set and to "test" to generate the test set.')
+args = parser.parse_args()
+
+# Limits
+rotations = np.linspace(-20, 20, 11)
+sizes = np.linspace(10, 28, 21)
+fonts = ['courier new', 'dejavu sans mono', 'times new roman', 'arial',
+         'arial black', 'verdana', 'comic sans ms', 'georgia',
+         'liberation serif', 'impact', 'roboto condensed']
+
+# Use the SUBTLEX-US database to select words to plot
+subtlex_us = pd.read_table('subtlex-us.txt', index_col=0)
+
+# Select words between 2 and 6 letters long
+subtlex_us = subtlex_us[subtlex_us.index.str.len() <= 6]
+subtlex_us = subtlex_us[subtlex_us.index.str.len() >= 2]
+
+# Drop words with capitals (like names)
+subtlex_us = subtlex_us[subtlex_us.index.str.lower() == subtlex_us.index]
+
+# Use top 1000 most frequent words
+subtlex_us.sort_values('FREQcount', inplace=True, ascending=False)
+words = subtlex_us.head(1000).index.values
+
+rng = np.random.RandomState(0)
+
+chosen_rotations = []
+chosen_sizes = []
+chosen_fonts = []
+chosen_words = []
+
+plt.close('all')
+for word in tqdm(words, total=len(words)):
+    path = op.join(args.path, word)
+    makedirs(path)
+    n = 200 if args.set == 'train' else 50
+    for i in range(n):
+        dpi = 96.
+        f = plt.figure(figsize=(128 / dpi, 128 / dpi), dpi=dpi)
+        ax = f.add_axes([0, 0, 1, 1])
+
+        rotation = rng.choice(rotations)
+        fontsize = rng.choice(sizes)
+        family = rng.choice(fonts)
+        ax.text(0.5, 0.5, word, ha='center', va='center',
+                rotation=rotation, fontsize=fontsize, family=family)
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.axis('off')
+        plt.savefig(path + '/%03d.JPEG' % i)
+        plt.close()
+
+        chosen_words.append(word)
+        chosen_rotations.append(rotation)
+        chosen_sizes.append(fontsize)
+        chosen_fonts.append(family)
+
+df = pd.DataFrame(dict(word=chosen_words, rotation=chosen_rotations,
+                       size=chosen_sizes, font=chosen_fonts))
+df.to_csv('word_stimuli_%s.csv' % args.set)
---
+++ reading_model.py
@@ -0,0 +1,203 @@
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from torchvision import transforms, datasets
+from PIL import Image, ImageDraw, ImageFont
+
+# Device configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#device = 'cpu'
+
+# Hyper parameters
+num_epochs = 3
+batch_size = 20
+learning_rate = 0.001
+
+def select_channel(X):
+    """Our images are grayscale. Just select the first color channel."""
+    return X[[0]]
+
+def to_grayscale(X):
+    """Convert image to grayscale"""
+    return X.mean(dim=0).unsqueeze(0)
+
+train_dataset = datasets.ImageFolder(
+    root='/l/vanvlm1/large_word_stimuli/train',
+    transform=transforms.Compose([transforms.ToTensor(), select_channel])
+)
+
+val_dataset = datasets.ImageFolder(
+    root='/l/vanvlm1/large_word_stimuli/val',
+    transform=transforms.Compose([transforms.ToTensor(), select_channel])
+)
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, num_workers=4
+)
+
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=batch_size, num_workers=4
+)
+
+num_classes = len(train_dataset.classes)
+
+class ConvNet(torch.nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+
+        n_filters1 = 16
+        n_filters2 = 16
+        n_filters3 = 16
+        self.features = torch.nn.Sequential(
+            torch.nn.Conv2d(1, n_filters1, kernel_size=3, stride=1, padding=0),
+            torch.nn.BatchNorm2d(n_filters1),
+            torch.nn.ReLU(inplace=True),
+            #torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Conv2d(n_filters1, n_filters2, kernel_size=3, stride=1, padding=0),
+            torch.nn.BatchNorm2d(n_filters2),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Conv2d(n_filters2, n_filters3, kernel_size=3, stride=1, padding=0),
+            torch.nn.BatchNorm2d(n_filters3),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2))
+        self.classifier = torch.nn.Linear(n_filters3 * 30 * 30, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+            elif isinstance(m, torch.nn.BatchNorm2d):
+                torch.nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.bias, 0)
+        
+    def forward(self, X):
+        out = self.features(X)
+        out = out.reshape(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+def accuracy(y_hat, y):
+    with torch.no_grad():
+        acc = torch.mean((y_hat.argmax(dim=1) == y).to(torch.float32))
+    return acc.item()
+
+model = ConvNet().to(device)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train on the training set
+for epoch in range(num_epochs):
+    num_batches = len(train_dataloader)
+    for i, (X, y) in enumerate(train_dataloader):
+        X = X.to(device)
+        y = y.to(device)
+
+        y_hat = model(X)
+        loss = criterion(y_hat, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % 10 == 0:
+            print('Epoch: %d/%d  Train batch: %04d/%d  LR: %f  Loss: %.4f  Acc: %.4f' %
+                  (epoch + 1, num_epochs, i, num_batches, learning_rate, loss.item(), accuracy(y_hat, y)))
+    learning_rate /= 10
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Test on the validation set
+num_batches = len(val_dataloader)
+y_hat_val = np.zeros((num_batches, batch_size, num_classes))
+y_val = np.zeros((num_batches, batch_size))
+for i, (X, y) in enumerate(val_dataloader):
+    y_hat = model(X.to(device))
+    y_hat_val[i] = y_hat.cpu().detach().numpy()
+    y_val[i] = y.cpu().detach().numpy()
+    #print('Validation batch %03d/%d  Loss: %.4f  Acc: %.4f' % (i, num_batches, loss.item(), accuracy(y_hat, y.to(device))))
+y_hat_val = y_hat_val.reshape(-1, num_classes)
+y_val = y_val.ravel()
+print('Final score:', np.mean(y_hat_val.argmax(axis=1) == y_val))
+
+modulelist = list(model.features.modules())[1:] + list(model.classifier.modules())
+def dd_helper(layer, iterations, lr, img=None):
+    if img is None:
+        input = torch.autograd.Variable(torch.rand(1, 1, 64, 64).cuda(), requires_grad=True)
+    else:
+        input = torch.autograd.Variable(img.unsqueeze(0).cuda(), requires_grad=True)
+    model.zero_grad()
+    for i in range(iterations):
+#         print('Iteration: ', i)
+        out = input
+        for j in range(layer):
+            out = modulelist[j](out)
+        loss = out.norm()
+        loss.backward()
+        input.data = input.data + lr * input.grad.data
+    
+    return input.cpu().detach().numpy()[0]
+
+def vis_layer(layer, img, num_cols=8):
+    if type(img) == int:
+        X, y = train_dataset[img]
+    else:
+        X = img
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(X[0])
+
+    # Run the image through the layers
+    X2 = X.unsqueeze(0).to(device)
+    for i in range(layer + 1):
+        X2 = modulelist[i](X2)
+    X2 = X2.detach().cpu().numpy()
+
+    # Visualize the result
+    plot_kernels(X2[0], num_cols)
+
+def vis_weights(layer):
+    w = modulelist[layer].weight
+    plt.figure(3)
+    plt.clf()
+    plt.imshow(w[:, 0, :, :].detach().reshape(w.shape[0] * w.shape[2], w.shape[3]), cmap='gray_r')
+    plt.yticks(np.arange(0, w.shape[0] * w.shape[2], w.shape[2]))
+    plt.grid(axis='y')
+    plt.colorbar()
+
+def plot_kernels(tensor, num_cols=8):
+    if not tensor.ndim==3:
+        raise Exception("assumes a 3D tensor")
+    num_kernels = tensor.shape[0]
+    num_rows = 1+ num_kernels // num_cols
+    fig = plt.figure(2, figsize=(num_cols,num_rows))
+    plt.clf()
+    cmax = max(abs(tensor.min()), abs(tensor.max()))
+    cmin = -cmax
+
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+        im = ax1.imshow(tensor[i], cmap='RdBu_r')
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+        im.set_clim(cmin, cmax)
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+
+def text_phantom(text, size, fontsize=14, font='arial'):
+    # Create font
+    pil_font = ImageFont.truetype(
+        font + ".ttf", size=fontsize // len(text), encoding="unic")
+    text_width, text_height = pil_font.getsize(text)
+
+    # create a blank canvas with extra space between lines
+    canvas = Image.new('RGB', [size, size], (255, 255, 255))
+
+    # draw the text onto the canvas
+    draw = ImageDraw.Draw(canvas)
+    offset = ((size - text_width) // 2,
+              (size - text_height) // 2)
+    draw.text(offset, text, font=pil_font, fill="#000000")
+
+    # Convert the canvas into an array with values in [0, 1]
+    return torch.tensor(np.asarray(canvas)[None, :, :, 0] / 255.0, dtype=torch.float32).to(device)
