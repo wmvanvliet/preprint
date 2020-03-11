cc73bc2054feb7d7c3ee8d1cb66b0398ff0614f5
Marijn van Vliet <w.m.vanvliet@gmail.com>
2020-03-11 16:28:33 +0200

    Playing with semantic model

--- networks/reading.py
+++ networks/reading.py
@@ -139,27 +139,36 @@ class VGGSem(nn.Module):
         num_channels = state_dict['features.0.weight'].shape[1]
         prev_num_classes = state_dict['classifier.6.weight'].shape[0]
         classifier_size = state_dict['classifier.6.weight'].shape[1]
-
         vis_model = VGG16(num_channels, prev_num_classes, classifier_size)
-        vis_model.load_state_dict(state_dict)
 
-        if num_classes is not None:
+        if checkpoint['arch'] == 'vgg':
+            vis_model.load_state_dict(state_dict)
             if freeze:
-                print('=> freezing visual model')
+                print('=> freezing feature and classifier parts of the model')
                 for param in vis_model.parameters():
                     param.requires_grad = False
-
-        print(f'=> attaching semantic layer (going from {prev_num_classes} to {num_classes})')
-        return cls(vis_model, num_classes)
+            print(f'=> attaching semantic layer (going from {prev_num_classes} to {num_classes})')
+            model = cls(vis_model, num_classes)
+        elif checkpoint['arch'] == 'vgg_sem':
+            model = VGGSem(vis_model, num_classes, classifier_size)
+            model.load_state_dict(state_dict)
+            if freeze:
+                print('=> freezing all parts of the model')
+                for param in model.parameters():
+                    param.requires_grad = False
+        else:
+            raise ValueError(f"Invalid network architecture in checkpoint: {checkpoint['arch']}")
+        return model
 
     def __init__(self, vis_network, num_classes=300, classifier_size=1024):
         super().__init__()
-        self.vis = vis_network
-        num_words = vis_network.classifier[-3].weight.shape[0]
+        self.features = vis_network.features
+        self.classifier = vis_network.classifier
+        num_words = self.classifier[-3].weight.shape[0]
 
         # Stack on some semantic layers
         self.semantics = nn.Sequential(
-            nn.Linear(num_words, classifier_size),
+            nn.Linear(num_words, num_classes),
             #nn.ReLU(True),
             #nn.Dropout(),
             #nn.Linear(classifier_size, num_classes),
@@ -178,7 +187,9 @@ class VGGSem(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, X):
-        out = self.vis(X)
+        out = self.features(X)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
         out = self.semantics(out)
         return out
 
@@ -188,7 +199,7 @@ class VGGSem(nn.Module):
         batch_size = 128
         feature_outputs = []
         out = images
-        for i, layer in enumerate(self.vis.features):
+        for i, layer in enumerate(self.features):
             layer_out = []
             for j in range(0, len(out), batch_size):
                 layer_out.append(layer(out[j:j + batch_size]))
@@ -199,7 +210,7 @@ class VGGSem(nn.Module):
                 feature_outputs.append(out.detach().numpy().copy())
         classifier_outputs = []
         out = out.view(out.size(0), -1)
-        for i, layer in enumerate(self.vis.classifier):
+        for i, layer in enumerate(self.classifier):
             layer_out = []
             for j in range(0, len(out), batch_size):
                 layer_out.append(layer(out[j:j + batch_size]))
