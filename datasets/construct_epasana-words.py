d795ceda43736c5d047c257c7cfb8d92a51cbb97
oonareeta <60730040+oonareeta@users.noreply.github.com>
2020-06-08 10:40:31 +0300

    Update datasets/construct_epasana-words.py
    Co-authored-by: Marijn van Vliet <w.m.vanvliet@gmail.com>

--- datasets/construct_epasana-words.py
+++ datasets/construct_epasana-words.py
@@ -53,7 +53,7 @@ fonts = {
 }
 
 # Using the "epasana" stimulus list to select words to plot
-words = pd.read_csv(f'/m/nbe/scratch/epasana/stimuli.csv',sep=',')['text'][236:354]
+words = pd.read_csv(f'/m/nbe/scratch/epasana/stimuli.csv').query('type=="word"')['text']
 
 # Adding some common finnish words to pad the list to 200
 more_words = pd.read_csv(f'{data_path}/parsebank_v4_ud_scrambled.wordlist.txt', sep=' ', nrows=500, quoting=3, usecols=[1], header=None)
