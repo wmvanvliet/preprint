35221e33d006d0952a412b6d32eaf72fd259b8f9
Marijn van Vliet <w.m.vanvliet@gmail.com>
2020-07-08 16:53:42 +0300

    Working on epasana activity computations

--- epasana/epasana_compare_activity.py
+++ epasana/epasana_compare_activity.py
@@ -1,4 +1,5 @@
 import mne
+import mne_rsa
 import numpy as np
 import networks
 import torch
@@ -13,8 +14,16 @@ checkpoint = torch.load(f'../data/models/{model_name}.pth.tar', map_location='cp
 model = networks.vgg_sem.from_checkpoint(checkpoint, freeze=True)
 
 stimuli = pd.read_csv('stimulus_selection.csv')
-images = utils.get_stimulus_images(stimuli, data_path='../data/epasana')
+images = utils.get_stimulus_images(stimuli, data_path='/m/nbe/scratch/epasana')
 layer_outputs = model.get_layer_activations(images)
 layer_activity = []
+
 for output in layer_outputs:
-    layer_activity = output.sum(dim=
+    if output.ndim == 4:
+        layer_activity.append(np.square(output).sum(axis=(1, 2, 3)))
+    elif output.ndim == 2:
+        layer_activity.append(np.square(output).sum(axis=1))
+
+
+dsms = [mne_rsa.compute_dsm(a, metric='euclidean') for a in layer_activity]
+mne_rsa.plot_dsms(dsms, names=['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc1', 'fc2', 'word', 'semantic'], n_rows=2)
